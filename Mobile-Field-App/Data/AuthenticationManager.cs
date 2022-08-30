54b5b4a16fe011a9b82489ef72453fccfe29971e
Oscar Davies <oscar.william.davies@outlook.com>
2022-08-30 16:19:35 +0100

    Login To Field Portal

---
+++ Mobile-Field-App/Data/AuthenticationManager.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text.Json;
+using System.Threading.Tasks;
+using API_Whisperer;
+using GoogleGson;
+
+namespace Mobile_Field_App.Data
+{
+    public class AuthenticationManager
+    {
+        #region Fields
+
+        public Authentication authentication = new Authentication();
+        public System.Text.Json.JsonElement? userData;
+
+        #endregion Fields
+
+        #region Methods
+
+        public async Task<bool> ConfirmGrantInStorage()
+        {
+            var req = new Request();
+
+            req.method = "GET";
+            req.url = "auth/user/check";
+
+            var res = await req.Execute(authentication);
+            if (res.isSuccess)
+            {
+                userData = res.bodyAsJson.Value;
+                return true;
+            }
+            else
+            {
+                authentication.headers.Clear();
+                return false;
+            }
+        }
+
+        public async Task<bool> HasGrantInStorage()
+        {
+            var uid = await SecureStorage.Default.GetAsync("uid");
+            var token = await SecureStorage.Default.GetAsync("token");
+
+            if (uid != null && token != null)
+            {
+                authentication.headers.Clear();
+                authentication.headers.Add("uid", uid);
+                authentication.headers.Add("token", token);
+                return true;
+            }
+
+            return false;
+        }
+
+        public async Task<bool> PerformCredentialLogin(string email, string password)
+        {
+            var req = new Request();
+
+            req.method = "POST";
+            req.url = "auth/user/login";
+            req.headers.Add("email", email);
+            req.headers.Add("password", password);
+            req.headers.Add("bodyAuth", "true");
+
+            var res = await req.Execute();
+            if (res.isSuccess)
+            {
+                authentication.headers.Add("uid", res.bodyAsJson.Value.GetProperty("uid").GetString());
+                authentication.headers.Add("token", res.bodyAsJson.Value.GetProperty("token").GetString());
+                StoreGrant();
+                return true;
+            }
+
+            return false;
+        }
+
+        public async void StoreGrant()
+        {
+            foreach (var header in authentication.headers)
+            {
+                await SecureStorage.SetAsync(header.Key, header.Value);
+            }
+        }
+
+        #endregion Methods
+    }
+}
--- Mobile-Field-App/MauiProgram.cs
+++ Mobile-Field-App/MauiProgram.cs
@@ -1,27 +1,33 @@
-﻿using Microsoft.AspNetCore.Components.WebView.Maui;
+﻿using Microsoft.AspNetCore.Components;
+using Microsoft.AspNetCore.Components.WebView.Maui;
 using Mobile_Field_App.Data;
 
 namespace Mobile_Field_App;
 
 public static class MauiProgram
 {
-	public static MauiApp CreateMauiApp()
-	{
-		var builder = MauiApp.CreateBuilder();
-		builder
-			.UseMauiApp<App>()
-			.ConfigureFonts(fonts =>
-			{
-				fonts.AddFont("OpenSans-Regular.ttf", "OpenSansRegular");
-			});
+    #region Methods
 
-		builder.Services.AddMauiBlazorWebView();
-		#if DEBUG
-		builder.Services.AddBlazorWebViewDeveloperTools();
+    public static MauiApp CreateMauiApp()
+    {
+        var builder = MauiApp.CreateBuilder();
+        builder
+            .UseMauiApp<App>()
+            .ConfigureFonts(fonts =>
+            {
+                fonts.AddFont("OpenSans-Regular.ttf", "OpenSansRegular");
+            });
+
+        builder.Services.AddMauiBlazorWebView();
+#if DEBUG
+        builder.Services.AddBlazorWebViewDeveloperTools();
 #endif
-		
-		builder.Services.AddSingleton<WeatherForecastService>();
+        builder.Services.AddSingleton<AuthenticationManager>();
 
-		return builder.Build();
-	}
+        API_Whisperer.Request.urlStart = "https://portalapi.etherlive.com/api/";
+
+        return builder.Build();
+    }
+
+    #endregion Methods
 }
